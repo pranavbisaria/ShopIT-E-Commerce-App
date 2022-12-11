996440ac5d3a3c2f8c896a7efc6c89014b38028a
Pranav Bisaria <pranavbisariya29@gmail.com>
2022-12-12 01:30:27 +0530

    Initial Commit

---
+++ src/main/java/com/ShopIT/ShopItAppApplication.java
@@ -0,0 +1,53 @@
+package com.ShopIT;
+
+import com.ShopIT.Config.AppConstants;
+import com.ShopIT.Models.Role;
+import com.ShopIT.Repository.RoleRepo;
+import org.modelmapper.ModelMapper;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.List;
+
+@SpringBootApplication
+public class ShopItAppApplication implements CommandLineRunner {
+	private final RoleRepo roleRepo;
+
+	public ShopItAppApplication(RoleRepo roleRepo) {
+		this.roleRepo = roleRepo;
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopItAppApplication.class, args);
+	}
+
+	@Bean
+	public ModelMapper modelMapper(){
+		return new ModelMapper();
+	}
+
+	@Override
+	public void run(String... args){
+		try{
+			Role role1 = new Role();
+			role1.setId(AppConstants.ROLE_ADMIN);
+			role1.setName("ROLE_ADMIN");
+
+			Role role2 = new Role();
+			role2.setId(AppConstants.ROLE_NORMAL);
+			role2.setName("ROLE_NORMAL");
+
+			Role role3 = new Role();
+			role3.setId(AppConstants.ROLE_HOST);
+			role3.setName("ROLE_HOST");
+
+			List<Role> roles= List.of(role1, role2, role3);
+			this.roleRepo.saveAll(roles);
+		}
+		catch (Exception e){
+			e.printStackTrace();
+		}
+	}
+}
---
+++ src/test/java/com/ShopIT/ShopItAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ShopIT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopItAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

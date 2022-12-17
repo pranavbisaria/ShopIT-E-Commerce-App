ae9047fffe16ea0183db0429d59206c1fcf9b448
Pranav Bisaria <pranavbisariya29@gmail.com>
2022-12-17 22:06:14 +0530

    Added mail send on successful registration

--- src/main/java/com/ShopIT/Service/OTPService.java
+++ src/main/java/com/ShopIT/Service/OTPService.java
@@ -20,5 +20,16 @@ public class OTPService {
         this.emailService.sendEmail(subject, message, to);
         return otpCheck;
     }
-
+    public void SuccessRequest(String email, String name){
+        String subject = "Successfully registered on ShopIT";
+        String message = "Dear " + name + "," +
+                "\nThank you for registering on ShopIT" +
+                "\nNow enjoy the lowest price on product that you will never get before and enjoy 24/7 shopping experience."+
+                "\nPlace you first order now and enjoy the shopping on ShopIT" +
+                "\n(This is an auto generated email, so please do not reply back.)" +
+                "\nRegards," +
+                "\nTeam ShopIT";
+        String to = email;
+        this.emailService.sendEmail(subject, message, to);
+    }
 }

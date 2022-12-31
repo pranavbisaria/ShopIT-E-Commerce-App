0732b42fd849181951e4ed5bf92a04979e657744
Pranav Bisaria <pranavbisariya29@gmail.com>
2023-01-01 00:00:37 +0530

    Added twilio mobile OTP cache class

---
+++ src/main/java/com/ShopIT/Payloads/CategoryRepo.java
@@ -0,0 +1,8 @@
+package com.ShopIT.Payloads;
+import com.ShopIT.Models.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepo extends JpaRepository<Category, Integer> {
+}

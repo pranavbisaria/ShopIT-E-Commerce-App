fa65510ef44f78c6de9d38cc3575f27b82658545
Pranav Bisaria <pranavbisariya29@gmail.com>
2022-12-31 23:54:57 +0530

    Added Category Response Dto

---
+++ src/main/java/com/ShopIT/Payloads/Categories/CategoryDTO.java
@@ -0,0 +1,15 @@
+package com.ShopIT.Payloads.Categories;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class CategoryDTO {
+    private Integer categoryId;
+    private String categoryName;
+    private String imageName;
+}

5acff5b94d3eb25027dcab9772af9e4e1b7a006e
Pranav Bisaria <pranavbisariya29@gmail.com>
2022-12-31 23:50:45 +0530

    Added profile APIS

--- src/main/java/com/ShopIT/Config/AppConstants.java
+++ src/main/java/com/ShopIT/Config/AppConstants.java
@@ -1,5 +1,7 @@
 package com.ShopIT.Config;
 
+import com.twilio.type.PhoneNumber;
+
 public class AppConstants {
     public static final Integer ROLE_ADMIN = 1001;
     public static final Integer ROLE_NORMAL = 1002;
@@ -7,8 +9,10 @@ public class AppConstants {
     public static final long JWT_ACCESS_TOKEN_VALIDITY = 24 * 60 *60;
     public static final long JWT_REFRESH_TOKEN_VALIDITY = 100 * 24 * 60 *60;
     public static final Integer EXPIRE_MINs = 10;
+    public static final String secret = "jwtTokenKeyJwtTokenKeyJwtTokenKeyJwtTokenKeyJwtTokenKeyJwtTokenKeyjwtTokenKeyJwtTokenKey";
+    public static final String path = "https://elasticbeanstalk-ap-south-1-665793442236.s3.ap-south-1.amazonaws.com/resources/images/";
     public static final String GOOGLE_CLIENT_ID = "1084765789984-2q7ueo55fnj99vh3pleh36dt8giiopnv.apps.googleusercontent.com";
     public static final String GOOGLE_ISSUER = "https://accounts.google.com";
-    public static final String path = "https://elasticbeanstalk-ap-south-1-665793442236.s3.ap-south-1.amazonaws.com/resources/images/";
-    public static final String secret = "jwtTokenKeyJwtTokenKeyJwtTokenKeyJwtTokenKeyJwtTokenKeyJwtTokenKeyjwtTokenKeyJwtTokenKey";
+    public static  final String malePhoto = "https://elasticbeanstalk-ap-south-1-665793442236.s3.ap-south-1.amazonaws.com/resources/images/Male.jpg";
+    public static  final String femalePhoto = "https://elasticbeanstalk-ap-south-1-665793442236.s3.ap-south-1.amazonaws.com/resources/images/Female.jpg";
 }

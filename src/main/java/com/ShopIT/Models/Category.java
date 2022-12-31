c08dbb461b0a85c1066374be0d2bb1f2145b87ed
Pranav Bisaria <pranavbisariya29@gmail.com>
2022-12-31 23:53:37 +0530

    updated Category Model

--- src/main/java/com/ShopIT/Models/Category.java
+++ src/main/java/com/ShopIT/Models/Category.java
@@ -1,22 +1,20 @@
-package com.ShopIT.Models;//package com.ShopIT.Models;
-//
-//import lombok.AllArgsConstructor;
-//import lombok.Getter;
-//import lombok.NoArgsConstructor;
-//import lombok.Setter;
-//import jakarta.persistence.*;
-//import java.util.List;
-//@Getter@Setter
-//@NoArgsConstructor
-//@AllArgsConstructor
-//@Entity
-//public class Category {
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.SEQUENCE)
-//    private int categoryId;
-//    @Column(nullable = false)
-//    private String categoryName;
-//    private String imageName;
-//    @OneToMany(cascade = CascadeType.PERSIST)
-//    private List<Product> internshipsList;
-//}
+package com.ShopIT.Models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer categoryId;
+    @Column(nullable = false)
+    private String categoryName;
+    private String imageName;
+}

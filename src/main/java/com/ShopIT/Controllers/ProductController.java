2151ac3855c119e99cc23dd36d05c328e1bdee23
Pranav Bisaria <pranavbisariya29@gmail.com>
2023-01-02 15:09:48 +0530

    Added payments APIs

--- src/main/java/com/ShopIT/Controllers/ProductController.java
+++ src/main/java/com/ShopIT/Controllers/ProductController.java
@@ -1,94 +1,57 @@
 package com.ShopIT.Controllers;
-
-import com.ShopIT.Models.Images;
-import com.ShopIT.Models.PageResponse;
-import com.ShopIT.Models.User;
-import com.ShopIT.Payloads.PageableDto;
-import com.ShopIT.Payloads.Products.ProductDto;
+import com.ShopIT.Exceptions.ResourceNotFoundException;
+import com.ShopIT.Models.*;
+import com.ShopIT.Payloads.ApiResponse;
+import com.ShopIT.Payloads.PaymentReturnResponse;
 import com.ShopIT.Repository.ProductRepo;
 import com.ShopIT.Security.CurrentUser;
-import com.ShopIT.Service.ProductService;
+import com.ShopIT.Service.PaymentService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
+import com.razorpay.*;
+import java.security.SignatureException;
 import java.util.List;
-
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/products")
-public class ProductController {
-    private final ProductService productService;
+@RequestMapping("/payment")
+public class PaymentController {
     private final ProductRepo productRepo;
-
-//----------------------------------------------------Products-----------------------------------------------------------------------------
-
-//Add Product
-    @PreAuthorize("hasAnyRole('ADMIN', 'MERCHANT')")
-    @PostMapping("/add/{categoryId}")
-    public ResponseEntity<?> addProduct(@CurrentUser User user, @RequestPart("images") MultipartFile[] images, @RequestPart ProductDto productDto, @PathVariable("categoryId") Integer categoryId){
-        return this.productService.addProduct(user, images, productDto, categoryId);
+    private final PaymentService paymentService;
+    @PostMapping("/cart/createOrder")
+    public ResponseEntity<?> newPaymentFromCart(@CurrentUser User user) throws RazorpayException {
+        if(user.getProfile().getCart().getCartProducts() == null){
+            return new ResponseEntity<>(new ApiResponse("Invalid Action!!!", false), HttpStatus.BAD_REQUEST);
+        }
+        if(user.getProfile().getAddress()==null || user.getProfile().getAddress().isEmpty()){
+            return new ResponseEntity<>(new ApiResponse("Please Update Your Address first", false), HttpStatus.NOT_ACCEPTABLE);
+        }
+        List<ProductInCart> products = user.getProfile().getCart().getCartProducts();
+        long amt = 0L;
+        for (ProductInCart product : products) {
+            Product getProduct = product.getProduct();
+            amt += (long)(getProduct.getOriginalPrice()*(100-getProduct.getOfferPercentage())*product.getNoOfProducts());
+        }
+        if(amt == 0L){
+            return new ResponseEntity<>(new ApiResponse("Invalid Action!!!", false), HttpStatus.BAD_REQUEST);
+        }
+        return this.paymentService.createOrder(user, amt);
     }
-    @GetMapping("/get")
-    public ResponseEntity<?> getAllProducts(@RequestParam(value ="pageNumber", defaultValue = "0", required = false) Integer pageNumber,
-                                            @RequestParam(value ="pageSize", defaultValue = "5", required = false) Integer pageSize,
-                                            @RequestParam(value ="sortBy", defaultValue = "productId", required = false) String sortBy,
-                                            @RequestParam(value ="sortDir", defaultValue = "asc", required = false) String sortDir
-    ){
-        return new ResponseEntity<>(this.productService.getAllProducts(new PageableDto(pageNumber, pageSize, sortBy, sortDir)), HttpStatus.OK);
+    @PostMapping("/createOrder/{productId}")
+    public ResponseEntity<?> newPayment(@CurrentUser User user, @PathVariable("productId") Long productId) throws Exception {
+        Product product = this.productRepo.findById(productId).orElseThrow(()-> new ResourceNotFoundException("Product", "productId", productId));
+        long amt = (long)(product.getOriginalPrice()*(100-product.getOfferPercentage()));
+        if(user.getProfile().getAddress()==null || user.getProfile().getAddress().isEmpty()){
+            return new ResponseEntity<>(new ApiResponse("Please Update Your Address first", false), HttpStatus.NOT_ACCEPTABLE);
+        }
+        return this.paymentService.createOrder(user, amt);
     }
-    @GetMapping("/get/{productId}")
-    public ResponseEntity<?> getProductById(@PathVariable("productId") Long productId){
-        return this.productService.getProductById(productId);
+    @PostMapping("/update_order")
+    public ResponseEntity<?> updateOrder(@CurrentUser User user, @RequestBody PaymentReturnResponse response) throws SignatureException {
+        if(user.getProfile().getAddress()==null || user.getProfile().getAddress().isEmpty()){
+            return new ResponseEntity<>(new ApiResponse("Please Update Your Address first", false), HttpStatus.NOT_ACCEPTABLE);
+        }
+        return this.paymentService.updateOrder(user, response);
     }
-    @PreAuthorize("hasAnyRole('ADMIN', 'MERCHANT')")
-    @PutMapping("/updateProduct/{productId}")
-    public ResponseEntity<?> updateProduct(@CurrentUser User user, @PathVariable("productId") Long productId, @RequestPart ProductDto productDto){
-        return this.productService.updateProduct(user, productId, productDto);
-    }
-    @PreAuthorize("hasAnyRole('ADMIN', 'MERCHANT')")
-    @PatchMapping("/addImages/{productId}")
-    public ResponseEntity<?> addProductImages(@CurrentUser User user, @RequestParam(value = "images", required = false) MultipartFile[] images, @PathVariable("productId") Long productId){
-        return this.productService.AddProductImages(user, images, productId);
-    }
-    @PreAuthorize("hasAnyRole('ADMIN', 'MERCHANT')")
-    @PatchMapping("/removeImages/{productId}")
-    public ResponseEntity<?> removeProductImages(@CurrentUser User user, @RequestBody List<Images> imageUrls, @PathVariable("productId") Long productId){
-        return this.productService.deleteProductImages(user, imageUrls, productId);
-    }
-    @PreAuthorize("hasAnyRole('ADMIN', 'MERCHANT')")
-    @DeleteMapping("/deleteProduct/{productId}")
-    public ResponseEntity<?> deleteMapping(@CurrentUser User user, @PathVariable("productId") Long productId){
-        return this.productService.deleteProduct(user, productId);
-    }
-    @GetMapping("/getProductsByCategory/{categoryId}")
-    public ResponseEntity<?> getProductsByCategory(@PathVariable Integer categoryId,
-                                                   @RequestParam(value ="pageNumber", defaultValue = "0", required = false) Integer pageNumber,
-                                                   @RequestParam(value ="pageSize", defaultValue = "5", required = false) Integer pageSize,
-                                                   @RequestParam(value ="sortBy", defaultValue = "productId", required = false) String sortBy,
-                                                   @RequestParam(value ="sortDir", defaultValue = "asc", required = false) String sortDir
-    ){
-        return new ResponseEntity<>(this.productService.getAllProductByCategory(categoryId, new PageableDto(pageNumber, pageSize, sortBy, sortDir)), HttpStatus.OK);
-    }
-
-// ---------------------------------------------------------------CART----------------------------------------------------------------------------
-
-    @PostMapping("/addToCart/{productId}")
-    public ResponseEntity<?> addProductToCart(@CurrentUser User user, @PathVariable("productId") Long productId){
-        return this.productService.addProductToCart(user, productId);
-    }
-    @DeleteMapping("/removeFromCart/{productId}")
-    public ResponseEntity<?> removeProductFromCart(@CurrentUser User user, @PathVariable("productId") Long productId){
-        return this.productService.removeProductFromCart(user, productId);
-    }
-//    @PutMapping()
-//    public ResponseEntity<?> increaseQuantityInCart(){
-//
-//    }
-//    @PutMapping()
-//    public ResponseEntity<?> decreaseQuantityInCart(){
-//
-//    }
 }

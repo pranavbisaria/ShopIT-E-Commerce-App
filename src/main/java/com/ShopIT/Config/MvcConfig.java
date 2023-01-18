065fb4ef0d032c14e0fec4ee3eb592350c9f83b7
Pranav Bisaria <pranavbisariya29@gmail.com>
2023-01-18 23:47:39 +0530

    Updated Websocket config

--- src/main/java/com/ShopIT/Config/MvcConfig.java
+++ src/main/java/com/ShopIT/Config/MvcConfig.java
@@ -8,10 +8,24 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 @EnableWebMvc
 public class MvcConfig implements WebMvcConfigurer {
+    private static final String[] CLASSPATH_RESOURCE_LOCATIONS = {
+            "classpath:/META-INF/resources/", "classpath:/resources/",
+            "classpath:/static/", "classpath:/public/" };
+//    @Override
+//    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+//        if (!registry.hasMappingForPattern("/webjars/**")) {
+//            registry.addResourceHandler("/webjars/**").addResourceLocations(
+//                    "classpath:/META-INF/resources/webjars/");
+//        }
+//        if (!registry.hasMappingForPattern("/**")) {
+//            registry.addResourceHandler("/**").addResourceLocations(
+//                    CLASSPATH_RESOURCE_LOCATIONS);
+//        }
+//    }
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry
                 .addResourceHandler("/file/**")
-                .addResourceLocations("file:///"+ System.getProperty("user.dir") + "/target/classes/static/");
+                .addResourceLocations("classpath:/target/classes/static/");
     }
 }
--- src/main/java/com/ShopIT/Config/NotificationConfig.java
+++ src/main/java/com/ShopIT/Config/NotificationConfig.java
@@ -8,16 +8,16 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 
 @Configuration
 @EnableWebSocketMessageBroker
+//As its name suggests, @EnableWebSocketMessageBroker enables WebSocket message handling, backed by a message broker.
 public class NotificationConfig implements WebSocketMessageBrokerConfigurer {
     @Override
-    public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws");
-        registry.addEndpoint("/ws").withSockJS();
-    }
-
-    @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
-        registry.enableSimpleBroker("/all", "/particular");
-        registry.setApplicationDestinationPrefixes("/app");
+        registry.enableSimpleBroker("/topic");
+        registry.setApplicationDestinationPrefixes("/ws");
+    }
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/our-websocket");
+        registry.addEndpoint("/our-websocket").withSockJS();
     }
 }
